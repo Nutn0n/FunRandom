07be3167d4d0ab3504c23a7c10e56088bb0ac159
Nattanon Dungsunenarn <title409@icloud.com>
2017-06-19 22:39:29 +0700

    I can do it motherfucker!

--- js/index.js
+++ js/index.js
@@ -1,52 +1,22 @@
-function FunRandom() {
-
-    var item_array = [
-      {
-   id: 1,
-   title: 'iPhone',
-   info:'You will buy an iPhone',
-   img:'iphone.jpg'
- },
- {
-   id: 1,
-   title: 'iPad',
-   info:'You will buy an iPad',
-   img:'ipad.jpg'
-    },
-  {
-    id: 1,
-    title: 'Mac',
-    info:'You will buy a Mac',
-    img:'mac.jpg'
-  }
-    ];
-
-    this.generate = function() {
-        this.item = item_array[Math.floor(Math.random() * item_array.length)];
-    }
-
-    this.generate();
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+$scope.itemArray = [
+{
+title: 'iPhone',
+info:'You will buy an iPhone',
+img:'iphone.jpg'
+},
+{
+title: 'iPad',
+info:'You will buy an iPad',
+img:'ipad.jpg'
+},
+{
+title: 'Mac',
+info:'You will buy a Mac',
+img:'mac.jpg'
 }
-
-
-FunRandom.annotations = [
-    new ng.Component( {
-     selector: "random-meta"
-    }),
-    new ng.View({
-      template: `
-
-      <title>คุณจะได้ซื้อ {{item.title}} ในปีนี้</title>
-      <meta property="og:title"  content="คุณจะได้ซื้อ {{item.title}} "/>
-      <meta property="og:description"content=" {{item.info}} "/>
-      <meta property="og:image"   content=" img/{{item.img}} "/>
-        `
-    })
 ];
-
-
-document.addEventListener("DOMContentLoaded",
-    function() {
-        ng.bootstrap(FunRandom)
-    }
-)
+$scope.howMany = $scope.itemArray.length;
+$scope.item = $scope.itemArray[Math.floor(Math.random()*$scope.howMany)];
+});
