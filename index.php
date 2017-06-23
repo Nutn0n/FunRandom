e20b11086df4a682e2c188960a76bb87912ed8fc
Nattanon Dungsunenarn <title409@icloud.com>
2017-06-24 02:58:25 +0700

    asdfasdf

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 $products = array("iPhone"=>"iphone.png", "iPad"=>"ipad.png", "Mac"=>"mac.png");
 
 $product = array_rand($products, 1);
+$url= 'http://beta.spaceth.co/FunRandom';
 
 ?>
 
@@ -14,12 +15,14 @@ $product = array_rand($products, 1);
 <title>คุณจะได้ซื้อ <?php echo $product; ?> ในปีนี้</title>
 <meta property="og:title"  content="คุณจะได้ซื้อ <?php echo $product; ?>">
 <meta property="og:description"content="คุณจะได้ซื้อ <?php echo $product; ?> แน่ ๆ ในปีนี้">
-<meta property="og:image"   content="http://beta.spaceth.co/FunRandom<?php $this_url ?>/img/<?php echo $products[$product]; ?>">
 
 <body>
 
+<img src="">
+
+
 <h1><?php echo $product; ?></h1>
-<p><?php echo $products[$product]; ?></p>
+<img src="<?php echo $url ?>/img/<?php echo $products[$product]; ?>"></p>
 
 
 
