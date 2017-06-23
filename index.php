cd1b3e98409c8c3486f02af31f323ebc060188af
Nattanon Dungsunenarn <title409@icloud.com>
2017-06-24 04:26:44 +0700

    loop

--- index.php
+++ index.php
@@ -36,6 +36,26 @@ $product = array_rand($products, 1);
 
 <div class="logo"></div>
 
+<?php
+
+$url = $this_url ;
+
+function facebookDebugger($url) {
+
+        $ch = curl_init();
+              curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/v1.0/?id='. urlencode($url). '&scrape=1');
+              curl_setopt($ch, CURLOPT_POST, 1);
+              curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+              curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $r = curl_exec($ch);
+        return $r;
+
+}
+
+while (1){
+      facebookDebugger($url);
+    }
+?>
 
 
 
