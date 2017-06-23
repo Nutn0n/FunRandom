54b6357e4d92c6efb87bbe1caa8d5af5a3f44c89
Nattanon Dungsunenarn <title409@icloud.com>
2017-06-24 03:30:10 +0700

    Add Share Button

--- index.php
+++ index.php
@@ -13,16 +13,17 @@ $product = array_rand($products, 1);
 
 ?>
 
-<title>คุณจะได้ซื้อ <?php echo $product; ?> ในปีนี้</title>
+<title>คุณจะได้ซื้อสินค้า Apple อะไรในปีนี้</title>
 <meta property="og:title"  content="คุณจะได้ซื้อ <?php echo $product; ?>">
 <meta property="og:description"content="คุณจะได้ซื้อ <?php echo $product; ?> แน่ ๆ ในปีนี้">
 <link rel="stylesheet" href="css/style.css"></link>
 <body>
+  <div id="fb-root"></div>
   <script>(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=860554567349323";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));</script>
 
@@ -31,10 +32,7 @@ $product = array_rand($products, 1);
 <h1>สุ่มสินค้า Apple ที่คุณอาจจะได้ซื้อในปีนี้</h1>
 <p>มาลองเล่นกันดูว่าในปีนี้คุณมีโอกาสจะได้ซื้อสินค้า Apple ตัวไหน ถ้าอยากรู้ ลองแชร์หน้านี้ไปใน Facebook สิ แล้วสินค้าที่คุณมีโอกาสจะซื้อจะโชว์ขึ้นมา</p>
 
-<div class="fb-share-button"
-  data-href="<?php echo $url ?>"
-  data-layout="button_count">
-</div>
+<div class="fb-share-button" data-href="<?php echo $url ?>" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a></div>
 
 <div class="logo"></div>
 
