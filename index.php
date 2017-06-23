edaccca03340cb63d05a12407c92e386fb34e704
Nattanon Dungsunenarn <title409@icloud.com>
2017-06-24 03:07:56 +0700

    Add comments

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <html>
 <meta charset="UTF-8">
-
 <?php
 
+/*  Edit these variable */
 
+/*  Create array of your random items with their images */
 $products = array("iPhone"=>"iphone.png", "iPad"=>"ipad.png", "Mac"=>"mac.png");
-
-$product = array_rand($products, 1);
+/*  Enter the URL of this page. */
 $url= 'http://beta.spaceth.co/FunRandom';
+/*  This is how many of the items will be random ** Don't have to modify **/
+$product = array_rand($products, 1);
 
 ?>
-
 
 <title>คุณจะได้ซื้อ <?php echo $product; ?> ในปีนี้</title>
 <meta property="og:title"  content="คุณจะได้ซื้อ <?php echo $product; ?>">
@@ -18,8 +19,6 @@ $url= 'http://beta.spaceth.co/FunRandom';
 
 <body>
 
-
-<h1><?php echo $product; ?></h1>
 <img style="display:none; visibility:hidden; opacity:0;" src="<?php echo $url ?>/img/<?php echo $products[$product]; ?>"></p>
 
 
