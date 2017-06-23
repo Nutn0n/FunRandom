4634f2dfebdfddbc376a5eda957f2f3977737024
Nattanon Dungsunenarn <title409@icloud.com>
2017-06-24 02:51:14 +0700

    Refix URL

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 <meta charset="UTF-8">
 
 <?php
-$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$products = array("iPhone"=>"iPhone.png", "iPad"=>"iPad.png", "Mac"=>"Mac.png");
-$product = array_rand($products, 1);
+
+
+$this_url = "http://beta.spaceth.co/FunRandom/";
+
 ?>
 
 
 <title>คุณจะได้ซื้อ <?php echo $product; ?> ในปีนี้</title>
 <meta property="og:title"  content="คุณจะได้ซื้อ <?php echo $product; ?>">
 <meta property="og:description"content="คุณจะได้ซื้อ <?php echo $product; ?> แน่ ๆ ในปีนี้">
-<meta property="og:image"   content="<?php $actual_link ?>/img/<?php echo $products[$product]; ?> ">
+<meta property="og:image"   content="<?php $this_url ?>/img/<?php echo $products[$product]; ?> ">
 
 <body>
 
