e392ad2a5bca84191e5e0448e65f016d0d19a3ad
Nattanon Dungsunenarn <title409@icloud.com>
2017-06-24 02:49:10 +0700

    Use function to render link.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <meta charset="UTF-8">
 
 <?php
+$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $products = array("iPhone"=>"iPhone.png", "iPad"=>"iPad.png", "Mac"=>"Mac.png");
 $product = array_rand($products, 1);
 ?>
@@ -10,7 +11,7 @@ $product = array_rand($products, 1);
 <title>คุณจะได้ซื้อ <?php echo $product; ?> ในปีนี้</title>
 <meta property="og:title"  content="คุณจะได้ซื้อ <?php echo $product; ?>">
 <meta property="og:description"content="คุณจะได้ซื้อ <?php echo $product; ?> แน่ ๆ ในปีนี้">
-<meta property="og:image"   content="img/<?php echo $products[$product]; ?> ">
+<meta property="og:image"   content="<?php $actual_link ?>/img/<?php echo $products[$product]; ?> ">
 
 <body>
 
